087be93cdf52bd557d4a318ec3bcb3fa966e3504
jacopo calvi <jacopo.bob.calvi@gmail.com>
2026-07-07 16:26:10 +0100

    WIP - added favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import faviconLight from '$lib/assets/favicon-light.ico';
+	import faviconDark from '$lib/assets/favicon-dark.ico';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={faviconLight} media="(prefers-color-scheme: light)" />
+	<link rel="icon" href={faviconDark} media="(prefers-color-scheme: dark)" />
+</svelte:head>
 {@render children()}
