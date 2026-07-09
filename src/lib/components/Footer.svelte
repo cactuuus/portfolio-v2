b9b8c8e8bf35f7f578a87e7ec9d608654799f220
jacopo calvi <jacopo.bob.calvi@gmail.com>
2026-07-09 17:06:11 +0100

    feat(seo): add metadata, llms.txt, social preview tags

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,6 +4,12 @@
 	import { GITHUB_USERNAME, THIS_REPO_NAME } from '$lib/config';
 	import NormalLink from './NormalLink.svelte';
 
+	const LINK_TO_LLMS: Link = {
+		label: 'llms.txt',
+		url: '/llms.txt',
+		ownAsset: true,
+		newTab: false
+	};
 	const LINK_TO_REPO: Link = {
 		label: 'open on GitHub',
 		url: makeGithubUrl(GITHUB_USERNAME, THIS_REPO_NAME),
@@ -12,10 +18,17 @@
 	};
 </script>
 
-<footer class="font-mono text-xs text-base-content/80 flex flex-col md:flex-row md:gap-4 gap-2 p-4">
-	<div class="flex-1 md:text-left text-center hidden md:block"></div>
-	<div class="flex-1 text-center">&copy; {new Date().getFullYear()} Jacopo Calvi</div>
-	<div class="flex-1 md:text-right text-center">
+<footer
+	class="font-mono text-xs text-base-content/80 flex items-center flex-row md:gap-4 gap-2 p-4"
+>
+	<div class="flex-1 text-left items-start"></div>
+	<div class="flex-1 text-center items-center">
+		&copy; {new Date().getFullYear()} Jacopo Calvi
+	</div>
+	<div class="flex-1 text-right items-end flex flex-col gap-3 md:gap-1">
 		<NormalLink link={LINK_TO_REPO} classes="text-xs" />
+		<div class="tooltip tooltip-info tooltip-end" data-tip="Feed me to your LLM!">
+			<NormalLink link={LINK_TO_LLMS} classes="text-xs" />
+		</div>
 	</div>
 </footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,27 @@
 	import faviconDark from '$lib/assets/favicon-dark.ico';
 
 	let { children } = $props();
+
+	const title = 'Jacopo Calvi — Software Developer';
+	const description =
+		'Software developer, graduated from the University of Sussex with a First Class Honours degree in Computer Science. I tinker with many different technologies, with particular focus on web development.';
 </script>
 
 <svelte:head>
 	<link rel="icon" href={faviconLight} media="(prefers-color-scheme: light)" />
 	<link rel="icon" href={faviconDark} media="(prefers-color-scheme: dark)" />
+	<meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
+	<meta name="theme-color" content="#121c22" media="(prefers-color-scheme: dark)" />
+	<link rel="canonical" href="https://jacopocalvi.com" />
+	<title>{title}</title>
+	<meta name="description" content={description} />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content="https://jacopocalvi.com/og-image.jpg" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+	<meta property="og:url" content="https://jacopocalvi.com" />
+	<meta property="og:type" content="website" />
+	<meta name="twitter:card" content="summary_large_image" />
 </svelte:head>
 {@render children()}
